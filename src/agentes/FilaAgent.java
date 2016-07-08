4a37497d03f33c9f79b596c57261fc947c57749e
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2016-07-07 21:43:22 -0400

    [CLEAN] Classe filaagent vazia
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/agentes/FilaAgent.java
+++ src/agentes/FilaAgent.java
@@ -34,7 +34,6 @@ public class FilaAgent extends Agent {
 
         @Override
         protected void onTick() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
 
     }
@@ -42,79 +41,20 @@ public class FilaAgent extends Agent {
     private class FilaBehaviour extends CyclicBehaviour {
 
         public FilaBehaviour(Agent a) {
-            super(a);
         }
 
         @Override
         public void action() {
-
-            ACLMessage msg = myAgent.receive();
-            try {
-                if (msg != null) {
-                    ACLMessage reply = msg.createReply();
-                    if (msg.getPerformative() != ACLMessage.INFORM) {
-                        if (msg.getPerformative() == ACLMessage.REQUEST) {
-                            String content = msg.getContent();
-                            if (content != null) {
-                                myLogger.log(Logger.INFO, "Agent " + getLocalName() + " - Received Assignment Request from " + msg.getSender().getLocalName());
-                                if (content.startsWith("acesso=")) {
-//                                    Course course = CourseActions.loadByPK(Integer.parseInt(content.substring(content.indexOf("=") + 1)));
-//                                    reply.setPerformative(ACLMessage.INFORM);
-//                                    reply.setContent(UserActions.relatorioAcesso(
-//                                            RoleContextActions.getCourseStudents(course.getId()), course.getId()));
-                                } else if (content.startsWith("stats=")) {
-                                    reply.setPerformative(ACLMessage.INFORM);
-                                    reply.setContent("ok");
-                                }
-                            } else {
-                                myLogger.log(Logger.INFO, "Agent " + getLocalName() + " - Unexpected request [" + content + "] received from " + msg.getSender().getLocalName());
-                                reply.setPerformative(ACLMessage.REFUSE);
-                                reply.setContent("( UnexpectedContent (" + content + "))");
-                            }
-                        } else {
-                            myLogger.log(Logger.INFO, "Agent " + getLocalName() + " - Unexpected message [" + ACLMessage.getPerformative(msg.getPerformative()) + "] received from " + msg.getSender().getLocalName());
-                            reply.setPerformative(ACLMessage.NOT_UNDERSTOOD);
-                            reply.setContent("( (Unexpected-act " + ACLMessage.getPerformative(msg.getPerformative()) + ") )");
-                        }
-                        send(reply);
-                    }
-                } else {
-                    block();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    } // END of inner class WaitPingAndReplyBehaviour
-
-    @Override
-    protected void setup() {
-        // Registration with the DF 
-        DFAgentDescription dfd = new DFAgentDescription();
-        ServiceDescription sd = new ServiceDescription();
-        sd.setType("FilaAgent");
-        sd.setName(getName());
-        sd.setOwnership("soonho");
-        dfd.setName(getAID());
-        dfd.addServices(sd);
-        try {
-            DFService.register(this, dfd);
-            FilaBehaviour comportamento = new FilaBehaviour(this);
-            AtualizaBehaviour refresh = new AtualizaBehaviour(this, 500);
-            addBehaviour(comportamento);
-            addBehaviour(refresh);
-        } catch (FIPAException e) {
-            myLogger.log(Logger.SEVERE, "Agent " + getLocalName() + " - Cannot register with DF", e);
-            doDelete();
         }
     }
 
     @Override
+    protected void setup() {
+
+    }
+
+    @Override
     protected void takeDown() {
-        try {
-            DFService.deregister(this);
-        } catch (Exception e) {
-        }
     }
 
 }
