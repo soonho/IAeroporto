50aa674503d6464c9e81dbcaa863a68d729b870c
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2016-07-07 22:10:27 -0400

    [ADJUST] Removendo pasta de configuração do netbeans
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/agentes/FilaAgent.java
+++ src/agentes/FilaAgent.java
@@ -6,16 +6,6 @@
 package agentes;
 
 import jade.core.Agent;
-import jade.core.behaviours.CyclicBehaviour;
-import jade.core.behaviours.TickerBehaviour;
-import jade.domain.DFService;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
-import jade.domain.FIPAAgentManagement.ServiceDescription;
-import jade.domain.FIPAException;
-import jade.lang.acl.ACLMessage;
-import jade.util.Logger;
-import java.util.ArrayList;
-import pojo.Aviao;
 
 /**
  *
@@ -23,38 +13,9 @@ import pojo.Aviao;
  */
 public class FilaAgent extends Agent {
 
-    private Logger myLogger = Logger.getMyLogger(getClass().getName());
-    private static ArrayList<Aviao> filaPouso = new ArrayList();
-
-    private class AtualizaBehaviour extends TickerBehaviour {
-
-        public AtualizaBehaviour(Agent a, long period) {
-            super(a, period);
-        }
-
-        @Override
-        protected void onTick() {
-        }
-
-    }
-
-    private class FilaBehaviour extends CyclicBehaviour {
-
-        public FilaBehaviour(Agent a) {
-        }
-
-        @Override
-        public void action() {
-        }
-    }
-
     @Override
     protected void setup() {
-
-    }
-
-    @Override
-    protected void takeDown() {
+        System.out.println("Testando");
     }
 
 }
