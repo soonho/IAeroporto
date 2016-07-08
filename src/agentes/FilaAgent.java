189e43ab09a3570d2ab6b757c5ed64b60fa50c05
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2016-07-08 00:21:51 -0400

    [ADD] Classe de comportamento e chamada na classe filaAgent
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/agentes/FilaAgent.java
+++ src/agentes/FilaAgent.java
@@ -1,21 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package agentes;
 
 import jade.core.Agent;
 
-/**
- *
- * @author georg
- */
 public class FilaAgent extends Agent {
 
     @Override
     protected void setup() {
         System.out.println("Testando");
+
+        FilaBehaviour behaviour = new FilaBehaviour(this);
+        addBehaviour(behaviour);
     }
 
 }
