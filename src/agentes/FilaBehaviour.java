61c1aa21f03577cfc458eb68262250691523c123
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2016-07-09 19:17:52 -0400

    [ADJUST] Metodo para calcular o aviao mais proximo do destino
    Signed-off-by: Walter Nascimento <walter.nascimento.barroso@gmail.com>

--- src/agentes/FilaBehaviour.java
+++ src/agentes/FilaBehaviour.java
@@ -4,8 +4,45 @@ import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
+import java.util.ArrayList;
+import javafx.geometry.Point3D;
+import jdk.nashorn.internal.objects.Global;
+import pojo.Aviao;
 
 public class FilaBehaviour extends CyclicBehaviour {
+
+    public static ArrayList<Aviao> aviaoes = new ArrayList();
+
+    public static void addAviao(Aviao aviao) {
+        for (Aviao av : aviaoes) {
+            if (av.getNome().equals(aviao)) {
+                return;
+            }
+        }
+        aviaoes.add(aviao);
+    }
+
+    public static void printDistancia() {
+        double x, y, z, sqrt, menor = Global.Infinity;
+        for (Aviao av : aviaoes) {
+            System.out.println("Distancia: " + av.getLocalizacao());
+            x = Math.pow(av.getxLocalizacao(), 2);
+            y = Math.pow(av.getyLocalizacao(), 2);
+            z = Math.pow(av.getzLocalizacao(), 2);
+            sqrt = Math.sqrt(x + y + z);
+            if (menor > sqrt) {
+                menor = sqrt;
+            }
+            System.out.println("SQRT = " + sqrt);
+            System.out.println("MENOR = " + menor);
+        }
+    }
+
+    public static void printName() {
+        for (Aviao av : aviaoes) {
+            System.out.println("Nome: " + av.getNome());
+        }
+    }
 
     public FilaBehaviour(Agent a) {
         super(a);
@@ -13,9 +50,41 @@ public class FilaBehaviour extends CyclicBehaviour {
 
     public void informRadar(ACLMessage msg) {
         System.out.println("Recebendo Informação do RADAR: " + msg.getContent());
-        if (true) {
-            acionaGoldFinger();
-        }
+        Aviao aviao = new Aviao(
+                "Aviaozinhow",
+                new Point3D(4, -2, 3),
+                new Point3D(0, 0, 0),
+                Integer.parseInt("3"),
+                "Rapidinho",
+                Double.parseDouble("2"));
+        FilaBehaviour.addAviao(aviao);
+
+        Aviao aviao3 = new Aviao(
+                "Aviaozinhow",
+                new Point3D(14, 12, 13),
+                new Point3D(0, 0, 0),
+                Integer.parseInt("3"),
+                "Rapidinho",
+                Double.parseDouble("2"));
+        FilaBehaviour.addAviao(aviao3);
+//        System.out.println(new Point3D(4, -2, 3));
+
+        Aviao aviao2 = new Aviao(
+                "Aviaozinhow2",
+                new Point3D(3, 3, -3),
+                new Point3D(0, 0, 0),
+                Integer.parseInt("1"),
+                "lento",
+                Double.parseDouble("1"));
+        FilaBehaviour.addAviao(aviao2);
+
+//        System.out.println(new Point3D(3, 3, -3));
+        printDistancia();
+        printName();
+        System.out.println("OK");
+//        if (true) {
+//            acionaGoldFinger();
+//        }
     }
 
     public void requestControlador(ACLMessage msg) {
